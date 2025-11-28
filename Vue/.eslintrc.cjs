7e5ae6c64a910d3a42835b3572c469eb7f2f7ff0
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-11-28 14:11:06 +0200

    Fix parsing errors of tsconfig.json

--- Vue/.eslintrc.cjs
+++ Vue/.eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
     env: { es6: true },
     parserOptions: {
       project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
       'createDefaultProgram': true,
       'ecmaVersion': 2020,
     },
