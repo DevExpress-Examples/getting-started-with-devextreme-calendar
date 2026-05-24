54d9f62cb8ed68b7908edd758a253a869565e279
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 13:06:11 +0300

    fix(angular): add missing imports to standalone component

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { DxCalendarTypes } from 'devextreme-angular/ui/calendar';
 
 import { DxCalendarModule } from 'devextreme-angular/ui/calendar';
 
 @Component({
     selector: 'app-root',
-    imports: [DxCalendarModule],
+    imports: [DxCalendarModule, NgClass],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
