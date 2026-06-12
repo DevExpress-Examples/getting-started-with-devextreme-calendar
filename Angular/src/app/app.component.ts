770f76fda330d7621eed2578ff122b513bdac8dc
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 14:09:12 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { DxCalendarTypes } from 'devextreme-angular/ui/calendar';
 
@@ -8,6 +8,7 @@ import { DxCalendarModule } from 'devextreme-angular/ui/calendar';
     selector: 'app-root',
     imports: [DxCalendarModule, NgClass],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
